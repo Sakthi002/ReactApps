b36d3c45ec6c8aabd1ce52bd49dbc71cd2ab1a9a
Sakthivel Munusamy <106135104+Sakthi020@users.noreply.github.com>
2024-11-30 11:46:47 +0530

    Initial commit with multiple React projects

---
+++ first-react-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,19 @@
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+export default function CoreConcepts() {
+
+    return (
+        
+        <section id='core-concepts'>
+        
+          <h2>Core Concepts</h2>
+        
+          <ul>
+
+            {CORE_CONCEPTS.map((content,index)=>(
+              <CoreConcept key={index} {...content}/>
+            ))}
+          </ul>
+        </section>
+    );
+}
